ff85934a9be5ac99ded6a5a7296e5d8f4a2db834
Rahul6700 <rahulsk4087@gmail.com>
2025-03-17 09:51:44 +0530

    fix: sidebar issue

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,7 +5,7 @@ const Sidebar = () => {
 
   return (
     <div className="m-5">
-      <h1 className="text-4xl">Chronicle</h1>
+      <h1 className="text-4xl">Editor</h1>
       {array.map((key, index) => (
         <div key={index} className="bg-[#1e1e1e] w-[60%] my-10 p-2 rounded-xl text-white">
           <h6>{key}</h6>
